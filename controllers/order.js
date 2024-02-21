32cdf8a20c93786f173a3a3f72eb4dc2ef6ad5e6
acceleratorusa <acceleratorusa@gmail.com>
2024-02-22 04:07:27 +0500

    double

--- controllers/order.js
+++ controllers/order.js
@@ -46,9 +46,9 @@ exports.createOrder = asyncHandler(async (req, res, next) => {
     }
 
     const existTable = await Table.findOne({
-        _id: new mongoose.Types.ObjectId(table),
-        restaurant: new mongoose.Types.ObjectId(restaurant),
-        waiter: new mongoose.Types.ObjectId(id)
+        _id: table,
+        restaurant: restaurant,
+        waiter: id
     })
 
     if (!existTable) {
