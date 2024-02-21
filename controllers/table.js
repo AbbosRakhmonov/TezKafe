ddce63e4e30d38ed9567aa2a12ca2320e9d9ca74
acceleratorusa <acceleratorusa@gmail.com>
2024-02-21 22:19:31 +0500

    test 2

--- controllers/table.js
+++ controllers/table.js
@@ -232,7 +232,34 @@ exports.getTables = asyncHandler(async (req, res, next) => {
             $lookup: {
                 from: 'products',
                 let: {products: '$activeOrders.products.product'},
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {$in: ['$_id', '$$products']}
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 1,
+                            name: 1
+                        }
+                    }
+                ],
                 as: 'activeOrders.products'
+            }
+        },
+        {
+            $group: {
+                _id: '$_id', // Group by _id to reconstruct the array
+                typeOfTable: {$first: '$typeOfTable'},
+                name: {$first: '$name'},
+                waiter: {$first: '$waiter'},
+                archiveOrders: {$first: '$archiveOrders'},
+                totalOrders: {$first: '$totalOrders'},
+                activeOrders: {$push: '$activeOrders'}, // Push the modified activeOrders back into an array
+                activePrice: {$sum: '$activeOrders.totalPrice'}, // Recalculate activePrice and activeItems
+                activeItems: {$sum: '$activeOrders.quantity'}, // Calculate sum of quantity
+                totalPrice: {$sum: '$activeOrders.totalPrice'} // Calculate sum of totalPrice
             }
         },
         {
