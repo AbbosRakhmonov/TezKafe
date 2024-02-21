24e0021c3ef523b7fa3635840845b0169033b5c1
acceleratorusa <acceleratorusa@gmail.com>
2024-02-22 01:00:05 +0500

    hi

--- middleware/upload.js
+++ middleware/upload.js
@@ -16,28 +16,39 @@ const mergeChunks = async (fileName, totalChunks) => {
     const chunkDir = path.join(__dirname, "..", "temp");
     const mergedFilePath = path.join(__dirname, "..", "uploads");
 
+
     if (!fs.existsSync(mergedFilePath)) {
-        fs.mkdirSync(mergedFilePath);
+        await fs.promises.mkdir(mergedFilePath);
     }
 
     const writeStream = fs.createWriteStream(`${mergedFilePath}/${fileName}`);
+
+
     for (let i = 0; i < totalChunks; i++) {
         const chunkFilePath = `${chunkDir}/${fileName}.part_${i}`;
-        const chunkBuffer = await fs.promises.readFile(chunkFilePath);
-        writeStream.write(chunkBuffer);
-        fs.unlinkSync(chunkFilePath); // Delete the individual chunk file after merging
+        try {
+            const chunkBuffer = await fs.promises.readFile(chunkFilePath);
+            writeStream.write(chunkBuffer);
+            await fs.promises.unlink(chunkFilePath); // Delete the individual chunk file after merging
+        } catch (error) {
+            console.error(`Error processing chunk ${i}:`, error);
+            throw error;
+        }
     }
 
     writeStream.end();
 
+
+    // get the file
     let file = path.join(__dirname, "..", "uploads", fileName);
-    let newFileName = fileName.replace(/\.[^/.]+$/, "") + `-${new Date().toLocaleDateString('RU')}` + ".webp";
+    let newFileName = fileName.replace(/\.[^/.]+$/, "") + `-${new Date().toISOString()}` + (convertToWebp ? ".webp" : "");
     //     resize and save
     await sharp(file)
         .webp({quality: 90})
         .toFile(path.join(__dirname, "..", "uploads", newFileName));
     //     delete the original file
-    fs.unlinkSync(file);
+    await fs.promises.unlink(file);
+
     return newFileName;
 };
 
